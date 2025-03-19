c0d5904ffa8b3f38ead8d75ca1fe4e2b8046700d
nghia00200 <nghialmpi00200@gmail.com>
2025-03-19 09:49:57 +0700

    feat/ fixWaringProduct

--- Backend/Models/Product.cs
+++ Backend/Models/Product.cs
@@ -13,12 +13,12 @@ namespace Backend.Models
         public required string Gender { get; set; }
         public int CatId { get; set; }
         [ForeignKey(nameof(CatId))]
-        public Category? Category { get; set; }
+        public Category Category { get; set; } = new();
         public int BraId { get; set; }
         [ForeignKey(nameof(BraId))]
-        public Brand? Brand { get; set; }
-        public List<ProductSize>? ProductSizes { get; set; }
-        public List<ProductColor>? ProductColors { get;set; }
+        public Brand Brand { get; set; } = new();
+        public List<ProductSize> ProductSizes { get; set; } = new();
+        public List<ProductColor> ProductColors { get; set; } = new();
 
         public DateTime CreateAt { get; set; } = DateTime.UtcNow;
 
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());    
 builder.Services.AddCors();
